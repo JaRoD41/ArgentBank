1094fb63d55f62755e6bad9b7e3397c6a2414799
JaRoD41 <laurent.trinco@gmail.com>
2023-08-05 23:53:05 +0200

    update to cra and typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,77 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import logo from '../public/argentBankLogo.png'
 
 export default function Home(): JSX.Element {
 	return (
-		<main className={styles.main}>
-			<div className={styles.description}>
-				<p>
-					Get started by editing&nbsp;
-					<code className={styles.code}>src/app/page.tsx</code>
-				</p>
-				<div>
-					<a
-						href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						By{' '}
-						<Image src="/vercel.svg" alt="Vercel Logo" className={styles.vercelLogo} width={100} height={24} priority />
-					</a>
-				</div>
-			</div>
-
-			<div className={styles.center}>
-				<Image className={styles.logo} src="/next.svg" alt="Next.js Logo" width={180} height={37} priority />
-			</div>
-
-			<div className={styles.grid}>
-				<a
-					href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Docs <span>-&gt;</span>
-					</h2>
-					<p>Find in-depth information about Next.js features and API.</p>
-				</a>
-
-				<a
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Learn <span>-&gt;</span>
-					</h2>
-					<p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-				</a>
-
-				<a
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Templates <span>-&gt;</span>
-					</h2>
-					<p>Explore the Next.js 13 playground.</p>
-				</a>
-
-				<a
-					href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-					className={styles.card}
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<h2>
-						Deploy <span>-&gt;</span>
-					</h2>
-					<p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-				</a>
-			</div>
-		</main>
+		<nav className="main-nav">
+			<a className="main-nav-logo" href="">
+				<img className="main-nav-logo-image" src={logo} alt="Argent Bank Logo" />
+			</a>
+		</nav>
+		// 	<nav class="main-nav">
+		//   <a class="main-nav-logo" href="./index.html">
+		//     <img
+		//       class="main-nav-logo-image"
+		//       src="./img/argentBankLogo.png"
+		//       alt="Argent Bank Logo"
+		//     />
+		//     <h1 class="sr-only">Argent Bank</h1>
+		//   </a>
+		//   <div>
+		//     <a class="main-nav-item" href="./sign-in.html">
+		//       <i class="fa fa-user-circle"></i>
+		//       Sign In
+		//     </a>
+		//   </div>
+		// </nav>
+		// <main>
+		//   <div class="hero">
+		//     <section class="hero-content">
+		//       <h2 class="sr-only">Promoted Content</h2>
+		//       <p class="subtitle">No fees.</p>
+		//       <p class="subtitle">No minimum deposit.</p>
+		//       <p class="subtitle">High interest rates.</p>
+		//       <p class="text">Open a savings account with Argent Bank today!</p>
+		//     </section>
+		//   </div>
+		//   <section class="features">
+		//     <h2 class="sr-only">Features</h2>
+		//     <div class="feature-item">
+		//       <img src="./img/icon-chat.png" alt="Chat Icon" class="feature-icon" />
+		//       <h3 class="feature-item-title">You are our #1 priority</h3>
+		//       <p>
+		//         Need to talk to a representative? You can get in touch through our
+		//         24/7 chat or through a phone call in less than 5 minutes.
+		//       </p>
+		//     </div>
+		//     <div class="feature-item">
+		//       <img
+		//         src="./img/icon-money.png"
+		//         alt="Chat Icon"
+		//         class="feature-icon"
+		//       />
+		//       <h3 class="feature-item-title">More savings means higher rates</h3>
+		//       <p>
+		//         The more you save with us, the higher your interest rate will be!
+		//       </p>
+		//     </div>
+		//     <div class="feature-item">
+		//       <img
+		//         src="./img/icon-security.png"
+		//         alt="Chat Icon"
+		//         class="feature-icon"
+		//       />
+		//       <h3 class="feature-item-title">Security you can trust</h3>
+		//       <p>
+		//         We use top of the line encryption to make sure your data and money
+		//         is always safe.
+		//       </p>
+		//     </div>
+		//   </section>
+		// </main>
+		// <footer class="footer">
+		//   <p class="footer-text">Copyright 2020 Argent Bank</p>
+		// </footer>
 	)
 }
