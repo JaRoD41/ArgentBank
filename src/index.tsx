6fabc4a163327cdd032c28ebca68b3d345b124ce
JaRoD41 <laurent.trinco@gmail.com>
2023-08-15 14:11:42 +0200

    add redux base

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import store from './app/store'
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Provider store={store}>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Provider>
 )
