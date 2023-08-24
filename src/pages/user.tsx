bf7a2949c5f756c771f5c33ebbccefebf318c2c5
JaRoD41 <laurent.trinco@gmail.com>
2023-08-24 09:21:28 +0200

    add user page

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -5,7 +5,6 @@ function User() {
 	const userOne = { lastName: 'Stark', firstName: 'Tony' }
 	const userTwo = { lastName: 'Rogers', firstName: 'Steve' }
 	const isLoggedIn = useSelector((state: any) => state.auth.isLoggedIn)
-	console.log('utilisateur connecté ? ', isLoggedIn)
 	return (
 		<>
 			<HeaderLoggedIn name={userOne.lastName} />
@@ -14,11 +13,41 @@ function User() {
 					<h1>
 						Welcome back
 						<br />
-						{userOne.firstName} {userOne.lastName}
+						Tony Jarvis!
 					</h1>
 					<button className="edit-button">Edit Name</button>
 				</div>
-				
+				<h2 className="sr-only">Accounts</h2>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Checking (x8349)</h3>
+						<p className="account-amount">$2,082.79</p>
+						<p className="account-amount-description">Available Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Savings (x6712)</h3>
+						<p className="account-amount">$10,928.42</p>
+						<p className="account-amount-description">Available Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
+				<section className="account">
+					<div className="account-content-wrapper">
+						<h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+						<p className="account-amount">$184.30</p>
+						<p className="account-amount-description">Current Balance</p>
+					</div>
+					<div className="account-content-wrapper cta">
+						<button className="transaction-button">View transactions</button>
+					</div>
+				</section>
 			</main>
 		</>
 	)
